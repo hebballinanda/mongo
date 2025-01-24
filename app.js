86f21dd23aff9eb736f2e0654d4a25cd9c2a191d
hebballinanda <hebballinanda123@gmail.com>
2025-01-24 20:34:48 +0530

    added initial connection for both mongoose and mongodb

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { connect } = require('./mongoose-db');
+require('dotenv').config()
+
+const app = express();
+
+const startServer = async () =>{
+    try{
+        await connect();
+        app.listen(process.env.PORT||"3000",()=>{
+            console.log("Server is running in Port: ",process.env.PORT)
+        })
+    }
+    catch(err){
+        console.log("Error in connecting to database")
+        process.exit(1); // Exit the process if the database connection fails
+    }
+}
+
+startServer()
---
+++ mongo-db.js
@@ -0,0 +1,45 @@
+const { MongoClient } = require('mongodb');
+
+const url = process.env.MONGOURL || 'mongodb://localhost:27017/';
+const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+});
+
+let dbInstance;
+
+const connect = async () => {
+    try {
+        if (!dbInstance) {
+            console.log("Connecting to the database...");
+            await client.connect();
+            dbInstance = client.db(process.env.DBNAME); // Replace 'DBNAME' with your actual database name if not using environment variables
+            console.log("Connected to the database successfully.");
+        }
+        return dbInstance;
+    } catch (err) {
+        console.error("Failed to connect to the database:", err);
+        throw err;
+    }
+};
+
+function getDb (){
+    if (!dbInstance) {
+        throw new Error('Database not connected');
+    }
+    return dbInstance;
+}
+
+const disconnect = async () => {
+    try {
+        if (client) {
+            await client.close();
+            console.log("Database connection closed.");
+        }
+    } catch (err) {
+        console.error("Failed to close the database connection:", err);
+        throw err;
+    }
+};
+
+module.exports = { connect, getDb, disconnect };
